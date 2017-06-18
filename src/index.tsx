6968a635d3e58a5a686fd37419dece623f1daeac
i5on9i <gaedduck@gmail.com>
2017-06-18 22:23:03 +0900

    - add react-router-dom v4

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { HashRouter } from 'react-router-dom'
 
-import { Hello } from "./components/Hello";
 
-ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
-);
+
+import SignUp from "./components/SignUp";
+
+// ReactDOM.render(
+//     <Hello compiler="TypeScript" framework="React" />,
+//     document.getElementById("example")
+// );
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    ReactDOM.render(
+        <HashRouter>
+            <SignUp />
+        </HashRouter>
+        ,
+        document.getElementById('example'));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,8 @@ module.exports = {
   // dependencies, which allows browsers to cache those libraries between builds.
   externals: {
       "react": "React",
-      "react-dom": "ReactDOM"
+      "react-dom": "ReactDOM",
+      "react-router-dom": "ReactRouterDOM"
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin('common'),
